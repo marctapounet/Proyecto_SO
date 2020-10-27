672c404040b84ecf9fac0125a7fd84a06a397c75
Marc Tapounet <marc24congres@gmail.com>
2020-10-27 12:42:16 +0100

    Proyecto_v1

---
+++ Cliente/Cliente/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conectar = new System.Windows.Forms.Button();
+            this.desconectar = new System.Windows.Forms.Button();
+            this.UsuarioBox = new System.Windows.Forms.TextBox();
+            this.ContraseñaBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Iniciar = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Registrar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UserName = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // conectar
+            // 
+            this.conectar.Location = new System.Drawing.Point(12, 28);
+            this.conectar.Name = "conectar";
+            this.conectar.Size = new System.Drawing.Size(84, 31);
+            this.conectar.TabIndex = 5;
+            this.conectar.Text = "conectar";
+            this.conectar.UseVisualStyleBackColor = true;
+            this.conectar.Click += new System.EventHandler(this.conectar_Click_1);
+            // 
+            // desconectar
+            // 
+            this.desconectar.Location = new System.Drawing.Point(181, 28);
+            this.desconectar.Name = "desconectar";
+            this.desconectar.Size = new System.Drawing.Size(84, 31);
+            this.desconectar.TabIndex = 8;
+            this.desconectar.Text = "Desconectar";
+            this.desconectar.UseVisualStyleBackColor = true;
+            this.desconectar.Click += new System.EventHandler(this.desconectar_Click_1);
+            // 
+            // UsuarioBox
+            // 
+            this.UsuarioBox.Location = new System.Drawing.Point(46, 108);
+            this.UsuarioBox.Name = "UsuarioBox";
+            this.UsuarioBox.Size = new System.Drawing.Size(100, 20);
+            this.UsuarioBox.TabIndex = 9;
+            // 
+            // ContraseñaBox
+            // 
+            this.ContraseñaBox.Location = new System.Drawing.Point(46, 181);
+            this.ContraseñaBox.Name = "ContraseñaBox";
+            this.ContraseñaBox.Size = new System.Drawing.Size(100, 20);
+            this.ContraseñaBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(43, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(43, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "contraseña";
+            // 
+            // Iniciar
+            // 
+            this.Iniciar.Location = new System.Drawing.Point(46, 229);
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.Size = new System.Drawing.Size(75, 23);
+            this.Iniciar.TabIndex = 13;
+            this.Iniciar.Text = "Iniciar Sesion";
+            this.Iniciar.UseVisualStyleBackColor = true;
+            this.Iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(190, 229);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 23);
+            this.Salir.TabIndex = 14;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // Registrar
+            // 
+            this.Registrar.Location = new System.Drawing.Point(110, 294);
+            this.Registrar.Name = "Registrar";
+            this.Registrar.Size = new System.Drawing.Size(75, 23);
+            this.Registrar.TabIndex = 15;
+            this.Registrar.Text = "Registrar";
+            this.Registrar.UseVisualStyleBackColor = true;
+            this.Registrar.Click += new System.EventHandler(this.Registrar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(489, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Escribe edad, puntos y minutos en las casillas en ese orden";
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.Location = new System.Drawing.Point(500, 151);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(113, 17);
+            this.UserName.TabIndex = 18;
+            this.UserName.Text = "Dame User_Name";
+            this.UserName.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(607, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 20);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(717, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(80, 20);
+            this.textBox3.TabIndex = 21;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(830, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(72, 20);
+            this.textBox4.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 380);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Registrar);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Iniciar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ContraseñaBox);
+            this.Controls.Add(this.UsuarioBox);
+            this.Controls.Add(this.desconectar);
+            this.Controls.Add(this.conectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button conectar;
+        private System.Windows.Forms.Button desconectar;
+        private System.Windows.Forms.TextBox UsuarioBox;
+        private System.Windows.Forms.TextBox ContraseñaBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Iniciar;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button Registrar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox UserName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Cliente/Cliente/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void conectar_Click_1(object sender, EventArgs e)
+        {
+
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9200);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+        }
+
+
+
+        private void desconectar_Click_1(object sender, EventArgs e)
+        {
+            //Mensaje de desconexión
+            string mensaje = "0/";
+
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            string mensaje = "1/" + UsuarioBox.Text + "/" + ContraseñaBox.Text;
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg3 = new byte[80];
+            server.Receive(msg3);
+            string mensajevuelta;
+            mensajevuelta = Encoding.ASCII.GetString(msg3).Split('\0')[0];
+            if (mensajevuelta == "Correctol")
+            {
+                MessageBox.Show("Se ha logeado");
+
+            }
+            else
+                MessageBox.Show("Incorrecto");
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            string mensaje = "1/" + UsuarioBox.Text + "/" + ContraseñaBox.Text;
+            // Enviamos al servidor el nombre tecleado
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg3 = new byte[80];
+            server.Receive(msg3);
+            string mensajevuelta;
+            mensajevuelta = Encoding.ASCII.GetString(msg3).Split('\0')[0];
+            if (mensajevuelta == "Correctol")
+            {
+                MessageBox.Show("Sesion cerrada conrrectamente");
+
+            }
+            else
+                MessageBox.Show("Incorrecto");
+        }
+
+        private void Registrar_Click(object sender, EventArgs e)
+        {
+            /* if (conexion == 1)
+            {
+
+                string nombre = UsuarioBox.Text;
+                string pass = ContraseñaBox.Text;
+                
+
+                if ((nombre == "") || (pass == ""))
+                {
+                    MessageBox.Show("Tienes que rellenar los 3 campos, si no no podrás registrarte.");
+                }
+
+                else
+                {
+                    nombre = nombre.Replace("/", "");
+                    pass = pass.Replace("/", "");
+                    
+
+                    string mensaje = "8/" + nombre + "/" + pass;
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+                }
+            }
+            else
+                MessageBox.Show("Antes de registrarte, tienes que conectarte primero.");
+        }*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (UserName.Checked)
+            {
+                string mensaje = "1/" + textBox1.Text + "/" + textBox2.Text + "/" + textBox3.Text + "/" + textBox4.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("User Name del Jugador es: " + mensaje);
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
